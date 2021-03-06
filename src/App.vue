f8c8f30910f6e43e07b3565b37e34ad5757c2a0f
rapira777 <dmitry.ts@mail.ru>
2021-03-06 12:51:47 +0300

    npm i vue-router

--- src/App.vue
+++ src/App.vue
@@ -21,11 +21,19 @@ export default {
   data() {
     return {
       todos:[
-          {id: 1, title: 'Купить хлеб', completed: false},
-          {id: 2, title: 'Купить масло', completed: false},
-          {id: 3, title: 'Купить сыр', completed: false},
+          // {id: 1, title: 'Купить хлеб', completed: false},
+          // {id: 2, title: 'Купить масло', completed: false},
+          // {id: 3, title: 'Купить сыр', completed: false},
       ]
     }
+  },
+  mounted() {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=3')
+  .then(response => response.json())
+  .then(json => {
+      this.todos = json
+  }
+      )
   },
    methods: {
     removeTodo(id) {
